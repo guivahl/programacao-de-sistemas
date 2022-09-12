5b755c67d00cd19892a05361523592357052abe5
Fernanda Kipper <nanda.kipper@gmail.com>
2022-09-12 10:31:53 -0300

    feat: implement memory methods to store stack and source code

--- java/Components/Memory.java
+++ java/Components/Memory.java
@@ -1,8 +1,29 @@
 package Components;
 
-public class Memory {
-    short[] values;
-    int limit;
+import java.util.ArrayList;
 
-    public Memory(){}
+public class Memory {
+    ArrayList<String> values;
+    int size;
+
+    public Memory(int size){
+        this.size = size;
+        this.values = new ArrayList<String>();
+    }
+
+    public String getValue(int position){
+        return this.values.get(position);
+    }
+
+    public void setValue(int position, String value){
+        if(position < size){
+            this.values.set(position, value);
+        } else {
+            throw new IllegalArgumentException("Invalid position, out of memory range");
+        }
+    }
+
+    public void pushValue(String value){
+        this.values.add(value);
+    }
 }
