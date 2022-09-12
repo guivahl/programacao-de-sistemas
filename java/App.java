de1e7636afa926942e7671fe813fe77c04e76061
Fernanda Kipper <nanda.kipper@gmail.com>
2022-09-12 10:41:02 -0300

    feat: initialize app reading source code and memory

--- java/App.java
+++ java/App.java
@@ -1,14 +1,24 @@
-import Components.Memory;
-import Components.Register;
-import Components.Stack;
+import java.util.ArrayList;
+
+import Components.*;
 
 public class App {
-
     public static void main(String[] args) throws Exception {
-        Memory memory = new Memory();
-        Stack stack = new Stack();
+        Memory memory = new Memory(100);
+        Stack stack = new Stack(memory, 10);
         Register pc = new Register();
         Register acc = new Register();
-        //TODO: start project
+
+        // Lê o source code e inicializa a memória com o programa
+        SourceCodeHandler reader = new SourceCodeHandler();
+        String data = reader.readFile("source-code.txt");
+        ArrayList<String> words = reader.parseStringToWords(data);
+        for(String word : words) {
+            memory.pushValue(word);
+        }
+
+        // testando
+        stack.push("00000000000000101");
+        System.out.println(stack.pop());
     }
 }
