a589ca9bdb536666b37663d14fb6d293997b7b24
Afonso <afonsotc@gmail.com>
2022-10-10 06:01:42 -0300

    fix:sourceCodeValidator

--- src/vm/Components/SourceCodeHandler.java
+++ src/vm/Components/SourceCodeHandler.java
@@ -9,6 +9,7 @@ import java.util.Set;
 
 public class SourceCodeHandler {
     private static final int WORD_SIZE = 16;
+    private static final int OP_CODE_START_INDEX = 12;
     public String readFile(String filename) throws FileNotFoundException{
         String data = "";
         Path path = Path.of("src", "resources", filename);
@@ -34,7 +35,7 @@ public class SourceCodeHandler {
             words.add(word);
         }
 
-        validateSourceCode(words);
+        this.validateSourceCode(words);
 
         return words;
     }
@@ -42,51 +43,38 @@ public class SourceCodeHandler {
 
     private void validateSourceCode(ArrayList<String> words) {
         Set<String> sizeOneOperation = Set.of("1001","1011");
-        Set<String> sizeTwoOperation = Set.of("0010","0000","0101","0001","0100","1111","1010","0011","1110","1100","0111","0110","1000");
+        //Set<String> sizeTwoOperation = Set.of("0010","0000","0101","0001","0100","1111","1010","0011","1110","1100","0111","0110","1000");
         Set<String> sizeThreeOperation = Set.of("1101");
-        final int START_INDEX = 0;
-        final int OP_CODE_START_INDEX = 12;
+
 
 
         for (int index = 0; index < words.size();) {
+            String word = words.get(index);
 
-            // Verify word size
-            if (words.get(index).length() != WORD_SIZE) throw new IllegalArgumentException("Source Code: Wrong OPCODE size. Index: " + index );
+            this.validateOpCode(word, index);
 
-            // Verify beginning of OPCODE
-            if (!words.get(index).substring(START_INDEX,OP_CODE_START_INDEX).matches("^[0]{12}")) throw new IllegalArgumentException("Source Code: Wrong OPCODE format(beginning). Index: " + index);
-
-            // Manage different sized OPCODES
-            if (sizeOneOperation.contains(words.get(index).substring(OP_CODE_START_INDEX))) {
+            if (sizeOneOperation.contains(word.substring(OP_CODE_START_INDEX))) {
                 index++;
-            } else if (sizeTwoOperation.contains(words.get(index).substring(OP_CODE_START_INDEX))) {
-                validateSizeTwoOperation(words.get(index+1), index);
-                index += 2;
-            } else if (sizeThreeOperation.contains(words.get(index).substring(OP_CODE_START_INDEX))) {
-                validateSizeThreeOperation(words.get(index+1), words.get(index+2), index);
+            } else if (sizeThreeOperation.contains(word.substring(OP_CODE_START_INDEX))) {
+                this.validateInstruction(words.get(index+1), index);
+                this.validateInstruction(words.get(index+2), index);
                 index += 3;
+
             } else {
-                throw new IllegalArgumentException("Source Code: Wrong OPCODE format(end). Index: " + index );
+                this.validateInstruction(words.get(index+1), index);
+                index += 2;
             }
         }
     }
 
-    private void validateSizeTwoOperation(String word, int index) {
-        // 0's and 1's
-        if(!word.matches("^[01]+$")) throw new IllegalArgumentException("Source Code: Not 0's and 1's. Index: " + index);
-
-        // size
-        if(word.length() != WORD_SIZE) throw new IllegalArgumentException("Source Code: Wrong word size. Index: " + index);
-
+    private void validateOpCode(String word, int index){
+        if (word.length() != WORD_SIZE) throw new IllegalArgumentException("Source Code: Wrong OPCODE size. Index: " + index );
+        if (!word.substring(0,OP_CODE_START_INDEX).matches("^[0]{12}")) throw new IllegalArgumentException("Source Code: Wrong OPCODE format(beginning). Index: " + index);
+        if (!word.substring(OP_CODE_START_INDEX).matches("^[01]+$")) throw new IllegalArgumentException("Source Code: Wrong OPCODE format(end). Index: " + index );
     }
-
-    private void validateSizeThreeOperation(String wordOne, String wordTwo, int index) {
-        // 0's and 1's
-        if(!wordOne.matches("^[01]+$")) throw new IllegalArgumentException("Source Code: Not 0's and 1's. Index: " + index);
-        if(!wordTwo.matches("^[01]+$")) throw new IllegalArgumentException("Source Code: Not 0's and 1's. Index: " + index);
-        // size
-        if(wordOne.length() != WORD_SIZE) throw new IllegalArgumentException("Source Code: Wrong word size. Index: " + index);
-        if(wordTwo.length() != WORD_SIZE) throw new IllegalArgumentException("Source Code: Wrong word size. Index: " + index);
+    private void validateInstruction(String word, int index){
+        if(!word.matches("^[01]+$")) throw new IllegalArgumentException("Source Code: Not 0's and 1's in operand. Index: " + index);
+        if(word.length() != WORD_SIZE) throw new IllegalArgumentException("Source Code: Wrong operand size. Index: " + index);
     }
 }
 
