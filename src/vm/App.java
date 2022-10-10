6155490f7463012981a65fc9b393dc62d02a1e91
Fernanda Kipper <nanda.kipper@gmail.com>
2022-10-10 08:18:21 -0300

    fix: adjust app to start GUI

--- src/vm/App.java
+++ src/vm/App.java
@@ -1,5 +1,6 @@
 package vm;
 
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 import vm.Components.Memory;
@@ -7,33 +8,79 @@ import vm.Components.Register;
 import vm.Components.SourceCodeHandler;
 import vm.Components.Stack;
 import vm.Components.Assembler.Assembler;
+import vm.Components.*;
+import vm.Components.Logger.*;
 
-public class App {
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class App implements ActionListener {
+    private Logger logger;
+    private Memory memory;
+    private Stack stack;
+    private Register programCounter;
+    private Register stackPointer;
+    private Register accumulator;
+    private Register operationMode;
+    private Register instructionRegister;
+    private Register memoryAddressRegister;
+    private GUI gui;
+    private Assembler assembler;
+
+    public App(){
+        this.logger = new Logger();
+        this.memory = new Memory(logger);
+        this.stack = new Stack(memory, 10);
+
+        ArrayList<Register> registers = new ArrayList<Register>();
+        this.programCounter = initializeRegister("PC", 16, registers);
+        this.stackPointer = initializeRegister("SP", 16, registers);
+        this.accumulator = initializeRegister("ACC", 16, registers);
+        this.operationMode = initializeRegister("MOP", 8, registers);
+        this.instructionRegister = initializeRegister("RI", 16, registers);
+        this.memoryAddressRegister = initializeRegister("RE", 16, registers);
+
+        gui = new GUI(stack, registers, memory, logger);
+        logger.logMessage("machine components initialized successfully", Logger.SUCCESS_MESSAGE);
+
+        gui.runButton.addActionListener(this);
+    }
+
     public static void main(String[] args) throws Exception {
-        Memory memory = new Memory();
-        Stack stack = new Stack(memory, 10);
+        new App();
+    }
 
-        Register programCounter = new Register("PC", 16);
-        Register stackPointer = new Register("SP", 16);
-        Register accumulator = new Register("ACC", 16);
-        Register operationMode = new Register("MOP", 8);
-        Register instructionRegister = new Register("RI", 16);
-        Register memoryAddressRegister = new Register("RE", 16);
+    private static Register initializeRegister(String identifier, int size, ArrayList<Register> registerArray){
+        Register newRegister = new Register(identifier, size);
+        registerArray.add(newRegister);
 
-        Assembler assembler = new Assembler();
+        return newRegister;
+    }
+
+    private void run() {
+        this.gui.cleanTerminalPanel();
+        this.logger.logMessage("starting running", Logger.SUCCESS_MESSAGE);
+
+        this.assembler = new Assembler();
 
         // Lê o source code e inicializa a memória com o programa
-        SourceCodeHandler reader = new SourceCodeHandler();
-        String data = reader.readFile("source-code.txt");
-        ArrayList<String> words = reader.parseStringToWords(data);
-        for(String word : words) {
-            memory.pushValue(word);
+        SourceCodeHandler reader = new SourceCodeHandler(logger);
+        String data;
+        try {
+            data = reader.readFile("source-code.txt");
+            ArrayList<String> words = reader.parseStringToWords(data);
+            for(String word : words) {
+                memory.pushValue(word);
+            }
+        } catch (FileNotFoundException e) {
+            logger.logMessage("Source code not found", Logger.ERROR_MESSAGE);
         }
 
-        // testando
-        stack.push("0000000000000101");
-        System.out.println(memory.getValue(0));
-        System.out.println(stack.pop());
-        System.out.println(memory.getValue(0));
+        // TODO: chamar a classe Operations para executar código
+    }
+
+    // listener pro botão de rodar programa
+    public void actionPerformed(ActionEvent e) {
+        this.run();
     }
 }
