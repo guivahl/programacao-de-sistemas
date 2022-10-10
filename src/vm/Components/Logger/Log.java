7573d756d99c610dc4d69f2782f3b7bf4f44bb98
Fernanda Kipper <nanda.kipper@gmail.com>
2022-10-10 08:15:47 -0300

    feat: create logger class

---
+++ src/vm/Components/Logger/Log.java
@@ -0,0 +1,13 @@
+package vm.Components.Logger;
+
+import java.awt.Color;
+
+public class Log {
+    public String message;
+    public Color type;
+
+    public Log(String message, Color type) {
+        this.message = message;
+        this.type = type;
+    }
+}
