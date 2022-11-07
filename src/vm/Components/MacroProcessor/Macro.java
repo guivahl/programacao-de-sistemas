b9f6486e42100f8f44442063d93773b403e03997
bvolcan <brunovolcan@gmail.com>
2022-11-07 13:43:45 -0300

    style: removed copilot comments

--- src/vm/Components/MacroProcessor/Macro.java
+++ src/vm/Components/MacroProcessor/Macro.java
@@ -38,18 +38,6 @@ public class Macro {
         this.instructions = instructions;
     }
 
-    // Create a copy of the instructions of the macro and replace the parameters with the values
-    // passed in the call
-    // The parameters normally are letters
-    // The instructions where the parameters are replaced are words
-    // Example:
-    // ADD A
-    // In the above example, the "A" in "ADD" cannot be replaced with the value passed in the call
-    // Return the copy of the instructions
-    // Split the string of instructions by a space
-    // For each word in the instructions, check if it is a parameter
-    // If it is a parameter, replace it with the value passed in the call
-
     public ArrayList<String> replaceParameters(ArrayList<String> values) {
         ArrayList<String> newInstructions = new ArrayList<>();
         for (String instruction : instructions) {
@@ -72,9 +60,6 @@ public class Macro {
     }
 
     public ArrayList<Macro> expandNestedMacros(ArrayList<String> values) {
-        // Do the same thing as the replaceParameters method but for nested macros
-        // Paramters in the outer macro will be replaced also in the nested macro if the respectives
-        // paramters are present in the instructions of the nested macro
 
         ArrayList<Macro> macros = new ArrayList<>();
         for (Macro nestedMacro : nestedMacros) {
@@ -100,6 +85,4 @@ public class Macro {
         }
         return macros;
     }
-
-
 }
--- src/vm/Components/MacroProcessor/MacroProcessor.java
+++ src/vm/Components/MacroProcessor/MacroProcessor.java
@@ -12,58 +12,19 @@ public class MacroProcessor {
     ArrayList<String> originalData = new ArrayList<>();
     Reader reader;
 
-    // MacroProcessor is a class to process macros
-    // MacroProcessor reads a .ASM file and stores the instructions in a list of objects of type
-    // Macro
-    // MacroProcessor is responsible for replacing macros with their respective instructions
-    // Macros in the .ASM file starts with a "MCDEFN" and ends with a "MCEND"
-    // Macros are defined in the .ASM file as follows:
-    // MCDEFN
-    // <macro_name> <macro_parameters>
-    // <macro_instructions>
-    // MCEND
-    // Macros can be defined in a nested way
-    // As an example:
-    // MCDEFN
-    // <macro1_name> <macro1_parameters>
-    // MCDEFN
-    // <macro2_name> <macro2_parameters>
-    // <macro2_instructions>
-    // MCEND
-    // <macro1_instructions>
-    // MCEND
-    // Macros can be called in a nested way
-    // Macros can be called with parameters
-    // All macros must be stored in a list of objects of type Macro
-    // All macros must be replaced with their respective instructions replacing the parameters with
-    // the values passed in the call
-    // Macros will be defined before called
-    // Macros can be called multiple times
-    // MacroProcessor create a output .ASM file with the macros replaced with their respective
-    // instructions
-
     public MacroProcessor(String fileName) throws FileNotFoundException, IOException {
         reader = new Reader(fileName);
-        // Read line by line the .ASM file, detect macros, and store their instructions in macros
-        // If a macro is found, read the next line to get the macro name and parameters
-        // Read the next lines ans store then as instructions until a "MCEND" is found
-        // If a "MCEND" is found, store the macro in the list of macros
-        // If a "MCDEFN" is found, repeat the described process and store the macro as a nested
-        // macro in the outer macro
-        // If a "MCEND" is found after a second "MCDEFN", return to the outer macro
         storeMacros();
         replaceMacros();
         writeOutputFile("MASMAPRG.ASM");
     }
 
     private void storeMacros() {
-        // Method to do the described process in the constructor
         String line = reader.readLine();
 
         while (line != null) {
             if (line.equals("MCDEFN")) {
                 line = reader.readLine();
-                // Split the line in " " and "," to get the macro name and parameters
                 String[] macroNameAndParameters = line.split(" |,");
                 String macroName = macroNameAndParameters[0];
                 ArrayList<String> macroParameters = new ArrayList<>();
@@ -108,17 +69,6 @@ public class MacroProcessor {
     }
 
     private void replaceMacros() {
-        // Method to rewrite the originalData with the macros replaced with their respective
-        // instructions using the Macro's replaceParameters method
-        // Nested macros will be moved to the end of the list of macros as soon as the outer macro
-        // is called
-        // Nested macros will be replaced with their respective instructions using the Macro's
-        // replaceParameters method
-        // If tried to convert from ArrayList<Macro> to ArrayList<String> java will throw a error
-        // The Method replaceParameters only receave a ArrayList<String> as parameter
-
-
-
         for (int i = 0; i < originalData.size(); i++) {
             String line = originalData.get(i);
             String[] lineSplit = line.split(" ");
@@ -149,18 +99,6 @@ public class MacroProcessor {
         }
 
     }
-
-    // Method to write the originalData in a .ASM file
-    // Each line will be written in a new line
-
-    // private void writeOutputFile(String fileName) throws IOException {
-    // BufferedWriter writer = new BufferedWriter(new FileWriter(fileName));
-    // for (int i = 0; i < originalData.size(); i++) {
-    // writer.write(originalData.get(i));
-    // writer.newLine();
-    // }
-    // writer.close();
-    // }
 
     public void writeOutputFile(String fileName) throws IOException {
         Writer writer = new Writer(fileName);
